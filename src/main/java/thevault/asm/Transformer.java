f9f30d515f019e1be6cf2d45d6f8d70590d14d10
way2muchnoise <jens.baetens@live.be>
2015-08-07 19:33:20 +0200

    fix import to the correct class

--- src/main/java/thevault/asm/Transformer.java
+++ src/main/java/thevault/asm/Transformer.java
@@ -1,7 +1,7 @@
 package thevault.asm;
 
+import betterachievements.registry.Reference;
 import thevault.asm.obfuscation.ASMString;
-import thevault.reference.Reference;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.Opcodes;
