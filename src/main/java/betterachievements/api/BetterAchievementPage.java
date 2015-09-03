ce6d598c52aef0b7c1e0b56e16dd28ca4b86e59b
way2muchnoise <jens.baetens@live.be>
2015-09-03 20:37:20 +0200

    adds default implementations of the interfaces

---
+++ src/main/java/betterachievements/api/BetterAchievementPage.java
@@ -0,0 +1,58 @@
+package betterachievements.api;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
+import net.minecraftforge.common.AchievementPage;
+
+/**
+ * Default implementation of {@link IBetterAchievementPage}
+ */
+public class BetterAchievementPage extends AchievementPage implements IBetterAchievementPage
+{
+    public BetterAchievementPage(String name, Achievement... achievements)
+    {
+        super(name, achievements);
+    }
+
+    @Override
+    public boolean hasCustomBackGround()
+    {
+        return false;
+    }
+
+    @Override
+    public void drawBackground(int left, int top, float z, float scale)
+    {
+
+    }
+
+    @Override
+    public boolean setScaleOnLoad()
+    {
+        return false;
+    }
+
+    @Override
+    public float setScale()
+    {
+        return 0;
+    }
+
+    @Override
+    public float getMaxZoom()
+    {
+        return 0;
+    }
+
+    @Override
+    public float getMinZoom()
+    {
+        return 0;
+    }
+
+    @Override
+    public ItemStack getPageIcon()
+    {
+        return null;
+    }
+}
