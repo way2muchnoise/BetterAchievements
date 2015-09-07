f79ec6782b800a6d3578de157d21a8469357b9c4
way2muchnoise <jens.baetens@live.be>
2015-09-07 17:24:32 +0200

    fixing more blending issues

--- src/main/java/betterachievements/gui/GuiBetterAchievements.java
+++ src/main/java/betterachievements/gui/GuiBetterAchievements.java
@@ -217,6 +217,7 @@ public class GuiBetterAchievements extends GuiScreen
             AchievementPage page = this.pages.get(i);
             if (page == selected) continue;
             GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glEnable(GL11.GL_BLEND);
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             int j = (i - this.tabsOffset) * tabWidth;
             this.mc.getTextureManager().bindTexture(Resources.GUI.TABS);
