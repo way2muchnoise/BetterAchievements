c2bbb7c73878fe70713f7009d43e93c635fd1445
way2muchnoise <jens.baetens@live.be>
2016-03-02 20:00:46 +0100

    remove redundant package-infos

--- src/main/java/betterachievements/api/components/page/package-info.java
+++
@@ -1,4 +0,0 @@
-@API(owner = "BetterAchievements", provides = "BetterAchievements|API", apiVersion = "@MAJOR@.@MINOR@.@REVIS@")
-package betterachievements.api.components.page;
-
-import net.minecraftforge.fml.common.API;
