e620eb015eb5a838832c245d8bb4a8d8341063d8
way2muchnoise <jens.baetens@live.be>
2016-03-24 22:15:59 +0100

    update version reference in code

--- src/main/java/betterachievements/api/package-info.java
+++ src/main/java/betterachievements/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "BetterAchievements", provides = "BetterAchievements|API", apiVersion = "@MAJOR@.@MINOR@.@REVIS@")
+@API(owner = "BetterAchievements", provides = "BetterAchievements|API", apiVersion = "@VERSION@")
 package betterachievements.api;
 
 import net.minecraftforge.fml.common.API;
--- src/main/java/betterachievements/reference/Reference.java
+++ src/main/java/betterachievements/reference/Reference.java
@@ -10,10 +10,7 @@ public class Reference
     public static final String RESOURCE_ID = ID.toLowerCase();
 
     // Main version information that will be displayed in mod listing and for other purposes.
-    public static final String V_MAJOR = "@MAJOR@";
-    public static final String V_MINOR = "@MINOR@";
-    public static final String V_REVIS = "@REVIS@";
-    public static final String VERSION_FULL = V_MAJOR + "." + V_MINOR + "." + V_REVIS;
+    public static final String VERSION_FULL = "@VERSION@";
 
     // proxy info
     public static final String SERVER_PROXY = "betterachievements.proxy.CommonProxy";
